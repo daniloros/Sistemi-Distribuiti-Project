3a5c40d7807b165ae194d604fd05168e70c67db0
daniros <danilorosati96@gmail.com>
2023-11-07 18:12:19 +0100

    add NavBar

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png";
+import close from "./close.png";
+import open from "./open.png";
+import create2 from "./create2.png";
+import accountName from "./acountName.png";
+
+
+export default {
+  logo,
+  close,
+  open,
+  create2,
+  accountName,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 
 import { ChatAppProvider } from "../Context/ChatAppContext";
+import {NavBar} from "@/Components";
 
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
       <ChatAppProvider>
+          <NavBar/>
           <Component {... pageProps} />
       </ChatAppProvider>
   </div>
