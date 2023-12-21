4a8c6f2d6e82afeafb04426304160084f4fb43c9
daniros <danilorosati96@gmail.com>
2023-12-21 19:29:40 +0100

    change contract address

--- Context/constants.js
+++ Context/constants.js
@@ -1,5 +1,8 @@
 import chatAppJSON from "./ChatApp.json";
 
-export const ChatAppAddress = "0x54E00d9FFEc3Ef5DF69E163837A87D748970a59c";
+//export const ChatAppAddress = "0x54E00d9FFEc3Ef5DF69E163837A87D748970a59c"; // contratto da test
+export const ChatAppAddress = "0xb72AF22FF037151dd116CF23ea7597D6BCEa2027";
+
+
 
 export const ChatAppABI = chatAppJSON.abi;
